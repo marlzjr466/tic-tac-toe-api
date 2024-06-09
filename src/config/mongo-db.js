5693665454e7bafe5a5a0fa2a77ea8cf9e594c8d
marlzjr466 <marlzjrjr@gmail.com>
2024-06-09 09:39:38 +0800

    revert using environmental variable

--- src/config/mongo-db.js
+++ src/config/mongo-db.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 
 class Mongoose {
   constructor () {
-    this.url = 'mongodb+srv://marlzjr15:Langomez466!@cluster0.12o2hb9.mongodb.net/'
+    this.url = process.env.MONGODB_URI
     this.options = { useNewUrlParser: true }
     this.db = null
   }
 
   start () {
-    console.log('started connecting to mongo db...', this.url)
+    console.log('started connecting to mongo db...')
     mongoose.connect(this.url)
     this.db = mongoose.connection
     this.db.on('error', error => console.log('db connection error:', error))
